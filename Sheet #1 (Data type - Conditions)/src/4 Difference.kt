728cd8c840046d9006ecfe6e06a6f7570c0dbac1
iamnippon <mindbend91@gmail.com>
2024-06-20 03:05:41 +0600

    Added Q5

--- Sheet #1 (Data type - Conditions)/src/4 Difference.kt
+++ Sheet #1 (Data type - Conditions)/src/4 Difference.kt
@@ -1,6 +1,28 @@
 fun main() {
+
+    // Questions: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
     val input = readLine()!!
     val (A,B,C,D) = input.trim().split("\\s+".toRegex()).map { it.toInt() }
     val X = (A*B)-(C*D)
     println("Difference = "+X)
+
+    // input
+    // 1 2 3 4
+    //----------
+    // output
+    // Difference = -10
+
+    // input
+    // 2 3 4 5
+    //----------
+    // output
+    // Difference = -14
+
+    // input
+    // 4 5 2 3
+    //----------
+    // output
+    // Difference = 14
+
 }
