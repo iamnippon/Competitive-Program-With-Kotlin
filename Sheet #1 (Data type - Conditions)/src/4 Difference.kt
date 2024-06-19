6e6f29ebbadcef8bfd0c7774cad844e94655f544
iamnippon <mindbend91@gmail.com>
2024-06-20 01:39:58 +0600

    Added Q6

--- Sheet #1 (Data type - Conditions)/src/4 Difference.kt
+++ Sheet #1 (Data type - Conditions)/src/4 Difference.kt
@@ -2,5 +2,5 @@ fun main() {
     val input = readLine()!!
     val (A,B,C,D) = input.trim().split("\\s+".toRegex()).map { it.toInt() }
     val X = (A*B)-(C*D)
-    println("Difference = $X")
+    println("Difference = "+X)
 }
