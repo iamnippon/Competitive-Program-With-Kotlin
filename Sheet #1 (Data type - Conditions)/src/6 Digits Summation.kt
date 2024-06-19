f4f1d40de823eec8e14e52a14242d4e890d47a60
iamnippon <mindbend91@gmail.com>
2024-06-20 03:01:27 +0600

    Added Q6

--- Sheet #1 (Data type - Conditions)/src/6 Digits Summation.kt
+++ Sheet #1 (Data type - Conditions)/src/6 Digits Summation.kt
@@ -2,6 +2,7 @@ import kotlin.math.abs
 
 fun main(){
 
+    //Questions: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
     val input = readLine()!!
     val (x, y) = input.trim().split(" ".toRegex()).map { it.toLong() }
     val xLast = getLastDigit(x)
@@ -10,4 +11,12 @@ fun main(){
 }
 fun getLastDigit(number: Long): Long {
     return abs(number) % 10
+
+    // input
+    // 13 12
+    //----------
+    // output
+    // 5
+
+
 }
