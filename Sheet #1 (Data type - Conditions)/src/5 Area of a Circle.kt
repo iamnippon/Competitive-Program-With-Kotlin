1c103fdacbf34903dba6855cf5d8e74b6d53bc52
iamnippon <mindbend91@gmail.com>
2024-06-20 03:03:16 +0600

    Added Q5

--- Sheet #1 (Data type - Conditions)/src/5 Area of a Circle.kt
+++ Sheet #1 (Data type - Conditions)/src/5 Area of a Circle.kt
@@ -1,9 +1,18 @@
 
 fun main(){
+
+    // Questions : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+
     val R = readln().toDouble()
     val pi = 3.141592653
     val Area = (pi * R * R).also {
         println(it)
     }
+
+    // input
+    // 2.00
+    //----------
+    // output
+    // 12.566370612
 
 }
