2743c988849ce35463feb72bd9eae04fc49a56fb
iamnippon <mindbend91@gmail.com>
2024-06-20 02:55:10 +0600

    Added Q6

---
+++ Sheet #1 (Data type - Conditions)/src/7 Summation from 1 to N.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // Read the input from standard input
+    val input = readLine()!!
+    val n = input.toLong()
+
+    // Calculate the sum using the formula
+    val save = n * (n + 1) / 2
+
+    // Print the result
+    println(save)
+}
+
+
+
