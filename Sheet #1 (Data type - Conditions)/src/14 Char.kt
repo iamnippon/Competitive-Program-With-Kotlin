a4e4f0183b3a262684b8d421219f5dfd33062a5f
iamnippon <mindbend91@gmail.com>
2024-06-23 02:29:26 +0600

    Update Q14

---
+++ Sheet #1 (Data type - Conditions)/src/14 Char.kt
@@ -0,0 +1,11 @@
+fun main(){
+    // Question https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/N
+
+    val input = readln()
+    if (input == input.uppercase()){
+        println(input.lowercase())
+    } else{
+        println(input.uppercase())
+    }
+
+}
